2b85bdec3f877af07743e3724436cd31dba8b939
viex <carp_97@outlook.com>
2025-03-20 13:58:04 -0600

    mend

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Constelutions.github.io",
+  base: "/Constelutions/",
   plugins: [
     vue(),
     vueDevTools(),
